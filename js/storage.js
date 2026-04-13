694341cd076cb1df4b933189db4d7345a613318b
PeterIregi <iregipeter4@gmail.com>
2026-04-13 22:41:43 +0300

    modify the add func

--- js/storage.js
+++ js/storage.js
@@ -2,12 +2,10 @@
 const todolist2=JSON.parse(localStorage.getItem('todolist')) || [];
 
 // add items and due date to list
-function addTodo3(){
-    const inputElement=document.querySelector('.js-name-input-03');
-    const dateElement=document.querySelector('.js-due-date-input');
+function addTodo(){
+    const inputElement=document.querySelector('#taskInput');
     const element01={};
     element01.name=inputElement.value;
-    element01.dueDate=dateElement.value;
     todolist2.push(element01)
     inputElement.value='';
     //saves to local storage before exiting
